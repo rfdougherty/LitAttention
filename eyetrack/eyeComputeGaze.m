c3247a6a37bf4b4797be819a7e9bb687a732662d
Bob Dougherty <bobd@stanford.edu>
2013-12-12 15:44:39 -0800

    Fixed bug for degenerate cal data

--- eyetrack/eyeComputeGaze.m
+++ eyetrack/eyeComputeGaze.m
@@ -12,7 +12,9 @@ function gaze = eyeComputeGaze(rawCoords, cal)
 
 gaze = [rawCoords rawCoords.^2 ones(size(rawCoords,1),1)] * cal.mat;
 gaze = gaze(:,1:2);
-[junk,quad] = ismember(sign(gaze),cal.quadSign,'rows');
+signGaze = sign(gaze);
+signGaze(signGaze==0) = 1;
+[junk,quad] = ismember(signGaze,cal.quadSign,'rows');
 gaze = gaze + cal.quadScale(quad,:).*repmat(gaze(:,1).*gaze(:,2),1,2);
 
 return;
